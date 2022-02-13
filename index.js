aa70f8d05b1a7631edcf8da6e63c2a6f950a36d8
vivek <smartvivekyadav9@gmail.com>
2022-02-13 16:05:20 +0530

    sdg

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/index.html`)
 })
 
+
+
+
+app.post("/link",(req, res)=>{
+
+    
+})
+
 app.listen(PORT, () => {console.log("sever is live"); })
 
