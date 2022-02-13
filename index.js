45facfb7fbd244f7af3798651949816dcb218ec8
vivek <smartvivekyadav9@gmail.com>
2022-02-13 16:45:33 +0530

    sddgdg

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const youtube = require("ytdl-core")
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/index.html`)
 })
 
+app.post("/inputlink",(req, res)=>{
 
+ const inputlink = req.body.link
+console.log(inputlink); 
+    youtube.getInfo(inputlink).then((data)=>{
+        const thumbnail =  data.player_response.videoDetails.thumbnail.thumbnails[2].url
+        const videolink = data.player_response.streamingData.formats[1].url
 
-
-app.post("/link",(req, res)=>{
-
-    
+        res.send(`<center><img src=\"${thumbnail}\"></center><br><br><center><a href=\"${videolink}\">Download This Video </a></center><br><center><a href="/">Download More Videos</a></center>`)
+    })
+   
 })
 
 app.listen(PORT, () => {console.log("sever is live"); })
